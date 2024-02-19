24421af0d7c0c2e9289fd693b1e58d3c301c78d1
lamson <171004297@qq.com>
2024-02-19 17:51:04 +0800

    feat: 新增飞书警报功能

---
+++ src/Feishu/Config.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace WonderGame\EsNotify\Feishu;
+
+use WonderGame\EsNotify\Interfaces\ConfigInterface;
+use WonderGame\EsNotify\Interfaces\NotifyInterface;
+use EasySwoole\Spl\SplBean;
+
+class Config extends SplBean implements ConfigInterface
+{
+    /**
+     * WebHook
+     * @var string
+     */
+    protected $url = '';
+
+    /**
+     * 密钥
+     * @var string
+     */
+    protected $signKey = '';
+
+    /**
+     * 要@哪些人（Open ID 或 User ID）, true-所有人,false-谁也不鸟
+     * @var array|bool
+     */
+    protected $at = false;
+
+    public function setUrl($url)
+    {
+        $this->url = $url;
+    }
+
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    public function setSignKey($signKey)
+    {
+        $this->signKey = $signKey;
+    }
+
+    public function getSignKey()
+    {
+        return $this->signKey;
+    }
+
+    public function setAt($at)
+    {
+        $this->at = $at;
+    }
+
+    public function getAt()
+    {
+        return $this->at;
+    }
+
+    public function getNotifyClass(): NotifyInterface
+    {
+        return new Notify($this);
+    }
+}
